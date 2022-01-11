baf9290063be48a336a794c2bca83f4e8a11219b
Kevin Wang <kevqwang@gmail.com>
2022-01-10 22:08:00 -0500

    commit tree 6f5d9de73f11356a1c082fd7d8326fa41bf7bc2e

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = (x) => x.slice(0,2);
+const returnLastTwoDrivers = (x) => x.slice(x.length -2);
+
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Claudia'];
+
+
+console.log( returnFirstTwoDrivers(drivers));
+console.log( returnLastTwoDrivers(drivers));
+console.log('drivers: ', drivers);
+
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = i => {
+    return function(fare) {
+        return fare * i
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(arrayOfDrivers, returnDriversFunction)  {
+    if(returnDriversFunction === returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers(arrayOfDrivers);
+    }
+    else if(returnDriversFunction === returnLastTwoDrivers) {
+        return returnLastTwoDrivers(arrayOfDrivers);
+    }
+}
